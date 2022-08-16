e2607fe81f19bc0632f27a3eea2ff7f6ee0ac389
ZhuJunfei <zhujunfeisnow@163.com>
2022-08-16 15:46:35 +0800

    带参数的装饰器的使用, 以及通用装饰器的学习

--- chapter04ClosureDecoratorCopy/Demo02Decorator.py
+++ chapter04ClosureDecoratorCopy/Demo02Decorator.py
@@ -6,13 +6,26 @@ Author:       ZhuJunfei
 import time
 
 
-def func1():
+def spend_time(func):
+    def test_time(lalala):
+        start_time = time.time_ns()
+        func(lalala)
+        end_time = time.time_ns()
+        print(f'spent time: {((end_time - start_time) / 1000000.0):.3f} ms')
+
+    return test_time
+
+
+@spend_time  # func1 = spend_time(func1)
+def func1(lalala):
+    print('lalala = ', lalala)
     _sum = 0
     for i in range(1000000):
         _sum += i
     return _sum
 
 
+@spend_time  # func2 = spend_time(func2)
 def func2():
     _res = 0
     for i in range(1000000):
@@ -20,18 +33,33 @@ def func2():
     return _res
 
 
-def spend_time(func):
-    def test_time():
-        start_time = time.time_ns()
-        func()
-        end_time = time.time_ns()
-        print(f'spent time: {((end_time - start_time) / 1000000.0):.3f} ms')
-    return test_time
+def logger(filename:str):
+    def outer_decorator(func):
+        def decorator(*args, **kwargs):
+            with open(filename, 'a', encoding='utf-8') as f:
+                f.write(f'{func.__name__} 被调用了')
+            return func()
+
+        return decorator
+
+    return outer_decorator
+
+
+@logger('aaa.log')
+# 等同于logger = logger('aaa.log') -> 此时的logger是outer_decorator, 然后python 解释器调用outer_decorator(login)将函数名传递进去
+# 于是 login = logger(login)
+def login():
+    print('login is running')
+
+
+@logger('bbb.log')
+def register():
+    print('register is running')
 
 
 if __name__ == "__main__":
     # print(time.time_ns() / 1000000000.0)
-    func1 = spend_time(func1)
-    func1()
-    func2 = spend_time(func2)
-    func2()
+    # func1('乌拉~')
+    # func2()
+    login()
+    register()
